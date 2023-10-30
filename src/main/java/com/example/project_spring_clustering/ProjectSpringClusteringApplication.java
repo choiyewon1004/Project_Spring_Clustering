8d73d598d1eb0c96cfa3d5ad9dc5c5200e678a84
choiyewon <choiyewon1997@gmail.com>
2023-10-30 14:58:28 +0900

    Initial commit

---
+++ src/main/java/com/example/project_spring_clustering/ProjectSpringClusteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_clustering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringClusteringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringClusteringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_spring_clustering/ProjectSpringClusteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_clustering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringClusteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
